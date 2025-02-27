4f46a524438fddf11a09d35a4fb4d42c4270ee70
Lucky <lucky123@gmail.com>
2025-02-27 08:29:19 +0530

    loading

--- src/app/(auth)/callback/loading.tsx
+++ src/app/(auth)/callback/loading.tsx
@@ -1,11 +1,12 @@
+import { Loader2 } from 'lucide-react'
 import React from 'react'
 
-function Loading() {
+function AuthLoading() {
   return (
-    <div>
-      
+    <div  className=' flex h-screen w-full items-center  justify-center'>
+      <Loader2 className=' animate-spin' />
     </div>
   )
 }
 
-export default Loading
+export default AuthLoading
